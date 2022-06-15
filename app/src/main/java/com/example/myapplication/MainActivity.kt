fcfe2c12edf0a2d8d1916c0722cdb0e7764e3191
Imran <imran5683new@gmail.com>
2022-06-15 23:45:53 +0530

    fix 0.1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,23 +1,32 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var _binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val tv1 = findViewById<TextView>(R.id.tv1)
-        val btn = findViewById<Button>(R.id.btn1)
-        btn.setOnClickListener {
-            tv1.setText("Imran")
-        }
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
 
+        val userId = _binding.etUserName.text?.trim().toString()
+        val pass  = _binding.etPass.text?.trim().toString()
+
+        _binding.btnLogin.setOnClickListener {
+            val message = arrayListOf<String>(userId, pass)
+            val intent = Intent(this, MainActivity2::class.java).apply {
+                putExtra("data", message)
+            }
+            startActivity(intent)
+        }
 
     }
 }
